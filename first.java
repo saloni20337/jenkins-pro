6f27cffef1abc482516ce2f68801d171964f4ab0
Saloni kumari <salonikumari4726@gmail.com>
2025-10-21 18:04:23 +0530

    "pra"

--- first.java
+++ first.java
@@ -3,6 +3,6 @@ import java.util.*;
 
 public class first{
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("hello worl");
     }
 }
